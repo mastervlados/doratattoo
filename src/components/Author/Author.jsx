2f88026df3f4e4ff5c11a4f51439755656359502
Vladislav Elizarov <velizarovdev@gmail.com>
2026-04-27 13:35:00 +0300

    Add custom fonts

--- src/components/Author/Author.jsx
+++ src/components/Author/Author.jsx
@@ -4,10 +4,10 @@ import styles from './Author.module.scss'
 function Author(){
   return (
     <div>
-        <h2>Тату-мастер</h2>
-        <h1>Дарья Холод</h1>
+        <h2 className={styles.position}>Тату-мастер</h2>
+        <h1 className={styles.heading}>Дарья Холод</h1>
         <p className={styles.addressPosition}><Address/></p>
-        <p className={styles.slugPosition}>Создаю тату с характером — сюжетные, глубокие, графичные</p>
+        <p className={[styles.slugPosition, styles.slug].join(' ')}>Создаю тату с характером — сюжетные, глубокие, графичные</p>
         <p>Работаю по индивидуальным эскизам, с чистой проработкой деталей и вниманием к каждому клиенту</p>
     </div>
   )
